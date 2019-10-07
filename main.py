eaca8137a3229c22b550888ca2d4e9b3c6d1415d
Porechniy13 <porechniy13@yandex.ru>
2019-10-07 11:09:54 +0300

    Init

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-  import numpy as np
+import numpy as np
 
 class Neuron:
 	def __init__(self):
-		self.weight = random()
+		self.weight = []
+	
+	def initWeight(self, count):
+		for i in range(count):
+			self.weight[i] = random()
 	
 	def setInputValue(self, input):
 		self.input = input
@@ -13,22 +17,34 @@ class Neuron:
 class Layer: 
 	def __init__(self, neuronCount):
 		self.size = neuronCount
-		for i in range(0, self.size){
-			self.neuronList.append(Neuron())
-		}
+		self.neuronList = []
+		for i in range(self.size):
+			self.neuronList[i] = Neuron() 
 	
 	def setInputValue(self, inputValueArray):
-		for i in range(0, self.size){
+		for i in range(0, self.size):
 			self.neuronList[i].setInputValue(inputValueArray[i])
-		}
 
 class Network:
-	def __init__(self):
-		
-	def createNetwork(self):
+	def __init__(self, counter, neuronCount):
+		self.Layers = []
+		self.size = counter
+		for i in range(self.size):
+			self.Layers[i] = Layer(neuronCount[i])
 	
-	def launchNetwork(self):
-		
+	def networkInit():
+		for i in range(self.size):
+			for j in range(0,self.Layers[i].size-1):
+				self.Layers[i].neuronList[j].initWeigth(self.Layers[i+1].size)			
+	
+	def printNetwork():
+		for i in range(self.size):
+			for j in range(self.Layers[i].size):
+				print(self.Layers[i].neuronList[j].weight)
+	
 if __name__ == ("__main__"):
-	myNetwork = Network()
+	neuronsCounter = [4,5,1]
+	myNetwork = Network(len(neuronsCounter), neuronsCounter)
+	myNetwork.networkInit()
+	myNetwork.printNetwork()
 	
